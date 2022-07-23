0b2a83155577e53761cf4dfd887cb5f3cd1032ee
Abhishek <abhishekdhar1991@gmail.com>
2022-07-24 00:09:57 +0530

    modified:   index.php new file:   partials/_dbconnect.php

--- index.php
+++ index.php
@@ -16,8 +16,47 @@
 include 'partials/_header.php';
 ?>
 
-<div class="container">
-    <h2>Forum - Categories</h2>
+<div class="container my-3">
+    <h2 class="text-center">Browse Categories</h2>
+    <div class=" row">
+        <!-- User a for loop to iterate through categories -->
+        <div class="col-md-4 my-2">
+            <div class="card" style="width: 18rem;">
+                <img src="https://www.liblogo.com/img-logo/ph424p5c6-php-logo-php-logo-transparent-png-stickpng.png"
+                    class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">View threads</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 my-2">
+            <div class="card" style="width: 18rem;">
+                <img src="https://www.liblogo.com/img-logo/ph424p5c6-php-logo-php-logo-transparent-png-stickpng.png"
+                    class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">View threads</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 my-2">
+            <div class="card" style="width: 18rem;">
+                <img src="https://www.liblogo.com/img-logo/ph424p5c6-php-logo-php-logo-transparent-png-stickpng.png"
+                    class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">View threads</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>;
 
 <?php
---
+++ partials/_dbconnect.php
@@ -0,0 +1,2 @@
+<?php
+// Script to connect to the database
