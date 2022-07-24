4723e08f284933396d078693e8206caa288c316d
Abhishek <abhishekdhar1991@gmail.com>
2022-07-24 22:12:44 +0530

    modified:   index.php modified:   partials/_dbconnect.php new file:   threads.php

--- index.php
+++ index.php
@@ -14,7 +14,9 @@
 </head>
 <?php
 include 'partials/_header.php';
+include 'partials/_dbconnect.php';
 ?>
+
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -47,44 +49,29 @@ include 'partials/_header.php';
 
 <div class="container my-3">
     <h2 class="text-center">Browse Categories</h2>
-    <div class=" row">
-        <!-- User a for loop to iterate through categories -->
-        <div class="col-md-4 my-2">
+    <div class="row my-3">
+        <!-- Fetch all the categories -->
+        <!-- User a loop to iterate through categories -->
+        <?php
+        $sql = "SELECT * FROM `categories`";
+        $result = mysqli_query($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            // echo $row['category_id'];
+            $cat = $row['category_name'];
+            $desc = $row['category_description'];
+            echo '<div class="col-md-4 my-2">
             <div class="card" style="width: 18rem;">
-                <img src="https://www.liblogo.com/img-logo/ph424p5c6-php-logo-php-logo-transparent-png-stickpng.png"
+                <img src="https://source.unsplash.com/500x400?' . $cat . ',coding"
                     class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
+                    <h5 class="card-title">' . $cat . '</h5>
+                    <p class="card-text">' . substr($desc, 0, 30) . '...</p>
                     <a href="#" class="btn btn-primary">View threads</a>
                 </div>
             </div>
-        </div>
-        <div class="col-md-4 my-2">
-            <div class="card" style="width: 18rem;">
-                <img src="https://www.liblogo.com/img-logo/ph424p5c6-php-logo-php-logo-transparent-png-stickpng.png"
-                    class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">View threads</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4 my-2">
-            <div class="card" style="width: 18rem;">
-                <img src="https://www.liblogo.com/img-logo/ph424p5c6-php-logo-php-logo-transparent-png-stickpng.png"
-                    class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">View threads</a>
-                </div>
-            </div>
-        </div>
+        </div>';
+        }
+        ?>
     </div>
 </div>;
 
